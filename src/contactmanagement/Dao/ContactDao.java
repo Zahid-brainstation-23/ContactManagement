6626b7e2c29fd726992700ff512a4ae602308e68
Md. Zahid Hasan <zahid.hasan@brainstation-23.com>
2023-03-22 19:18:41 +0600

    contactdao implemented

--- src/contactmanagement/Dao/ContactDao.java
+++ src/contactmanagement/Dao/ContactDao.java
@@ -11,21 +11,35 @@ import java.util.List;
 public class ContactDao {
 
     private Connection connection;
+    private Statement statement;
 
-    public ContactDao() {
+    public ContactDao() throws SQLException {
         connection = DatabaseConnection.getConnection();
-     
+        statement = connection.createStatement();
     }
 
     public List<Contact> getContacts() throws SQLException {
 
-        Statement stmt = connection.createStatement();
-        ResultSet rs = stmt.executeQuery("select * from contact");
+        ResultSet rs = statement.executeQuery("select * from contact");
         List<Contact> contacts = new ArrayList<Contact>();
         while (rs.next()) {
-            contacts.add(new Contact(rs.getString("number"),rs.getString("name")));
+            contacts.add(new Contact(rs.getString("number"), rs.getString("name")));
         }
         return contacts;
     }
 
+    public void saveContact(Contact contact) throws SQLException {
+        String sql = "INSERT INTO contact('number','name') VALUES ('" + contact.getNumber() + "', '" + contact.getName() + "')";
+        statement.executeUpdate(sql);
+    }
+
+    public void deleteContact(String number) throws SQLException {
+        String sql = "delete from contact where number='" + number + "';";
+        statement.executeUpdate(sql);
+    }
+
+    public void deleteContact(Contact contact) throws SQLException {
+        String sql = "delete from contact where number='" + contact.getNumber() + "' and name='" + contact.getName() + "';";
+        statement.executeUpdate(sql);
+    }
 }
